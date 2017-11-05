f1b65effb7c40175ce85bfd6d9d6cd761129895a
Sryncu <Sryncu@Sryncu-Komputer>
2017-11-05 22:37:22 +0100

    Wykonane

---
+++ src/Zadanie3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Zadanie3 {
+
+	public static void main(String[] args) {
+	
+		Scanner reader = new Scanner(System.in);
+		
+	    int a = reader.nextInt();
+		
+	    
+	    int licznik = 1;
+	    
+	    while ( licznik <= a ) {
+	    	
+	    	 System.out.println(licznik);
+	    
+	    	 licznik = licznik*2;
+	    	 
+	    	}
+		
+		
+
+		
+	}
+	
+}
