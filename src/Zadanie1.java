e9dedfa776b784360d76a95788f0f602dc201e3b
Sryncu <Sryncu@Sryncu-Komputer>
2017-11-08 20:12:17 +0100

    Zadanie1

--- src/Zadanie1.java
+++ src/Zadanie1.java
@@ -1,38 +1,30 @@
 import java.util.Scanner;
 
-public class Zadanie1 {
-
+	public class Zadanie1{
 	
 	public static void main(String[] args) {
-
-		
-	Scanner reader = new Scanner(System.in);
-    int liczba = reader.nextInt();
 	
-    int licznik = 1;
-    
-    while ( licznik <= liczba  ) {
-    	
-    	
-    	if ( licznik % 2 == 1 ) {
-    	
-    	 System.out.println(licznik);
-    	
-    		}
-    	
-    	 licznik++;
-    	 
-    	}
-    
-    
-    //Drobna poprawka
-    
-    
-    //Kolejna poprwka
-    
-    //I kolejna
+
+	Scanner odczyt = new Scanner(System.in);
+	
+	int a = odczyt.nextInt();
+
+	int b = 1;
+	
+	while (b <= a) {
+		
+		
+		if (b %2==1) {
+		 System.out.println(b);
+		}
+		 
+		 b++;
+				 
+	}
+	
+
 
 	}
-
 	
+
 }
